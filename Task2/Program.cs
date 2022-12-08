5e5d975485c266ce9da9ce81474d317b15ae66be
Waidazzas <waidazzas@gmail.com>
2022-12-08 22:01:39 +0200

    Add calculator logic

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -10,6 +10,22 @@ namespace Task2
     {
         static void Main(string[] args)
         {
+
+            try
+            {
+                Console.Write("Write a first number: ");
+                int NumberA = int.Parse(Console.ReadLine());
+                Console.Write("Write a second number: ");
+                int NumberB = int.Parse(Console.ReadLine());
+                int Result = NumberA + NumberB;
+                Console.WriteLine($"Sum is: {Result}");
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("You have entered an incorrect value!");
+                Console.ReadKey();
+            }
         }
     }
 }
